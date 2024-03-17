3d737da477035b46778e9c8b0d36594b3166e037
Aditya Pradhan <136620407+adityapradhan202@users.noreply.github.com>
2024-03-17 23:45:28 +0530

    Add files via upload

---
+++ app_banner_gifs.py
@@ -0,0 +1,13 @@
+# gif links
+
+makima_waifu = 'https://www.icegif.com/wp-content/uploads/2022/11/icegif-782.gif'
+
+zenitsu = 'https://static.wixstatic.com/media/6205b1_41d8070e78de4237aa211028a7df5ac7~mv2.gif/v1/fill/w_300,h_105,q_90/6205b1_41d8070e78de4237aa211028a7df5ac7~mv2.gif'
+
+tanjiro_thunder_breathing = 'https://64.media.tumblr.com/146dbe4fabe9970760e49d1dc995f30d/d5c4f47330e4dbb1-68/s540x810/d0c951d21a8030ae6633481a401fe0a0e8bb82d8.gif'
+
+banners_list = [
+   makima_waifu,
+   zenitsu,
+   tanjiro_thunder_breathing
+]
