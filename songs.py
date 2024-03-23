c272fb8edf06a05b9b45758b3cc561ee020e3093
Aditya Pradhan <136620407+adityapradhan202@users.noreply.github.com>
2024-03-23 16:02:32 +0530

    Add files via upload

---
+++ songs.py
@@ -0,0 +1,28 @@
+
+song_link = {
+    "hells_paradise_s1":'https://youtu.be/zPZjqy0gopI?feature=shared',
+    'demon_slayer_s1': 'https://youtu.be/YkJvHe3KK2c?feature=shared',
+    'mha_s1_peace_sign': 'https://youtu.be/oTNYCBWO42c?feature=shared',
+    'chainsaw_man_s1': 'https://youtu.be/dFlDRhvM4L0?feature=shared',
+    'jjk_s2': 'https://youtu.be/60A31DKGzAM?feature=shared',
+    'black_clover_2': 'https://youtu.be/mcXrLFZBciY?feature=shared',
+    'vinland_saga_s1': 'https://youtu.be/xEVcTStgA4A?feature=shared',
+    'vinland_saga_s2': 'https://youtu.be/nkTj2Hk6nEc?feature=shared',
+    'tokyo_revengers': 'https://youtu.be/aNYpNMNc0qY?feature=shared',
+    'mha_trap_musicality': 'https://youtu.be/clnOi7Kmk8M?feature=shared',
+    'uzui_theme': 'https://youtu.be/S4iGXg0QcrU?feature=shared',
+    'zenitsu_theme_1': 'https://youtu.be/1PO2AVXWBhk?feature=shared',
+    'zenitsu_theme_2': 'https://youtu.be/P8l2ZeOUZEA?feature=shared',
+    'rengoku_theme': 'https://youtu.be/SVhHhtG4DPM?feature=shared',
+    'loca_loca': 'https://youtu.be/eGpdyP9r9sU?feature=shared',
+    'suzume_theme': 'https://youtu.be/qal34e9v_pk?feature=shared',
+    'your_name': 'https://youtu.be/-pHfPJGatgE?feature=shared'
+}
+
+
+if __name__ == "__main__":
+
+    print((list(song_link.values()))[0])
+    
+
+
